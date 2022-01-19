f7426970237d1fc7980e3fd0ed4db6df4023bb45
bd21 <bd21@uw.edu>
2022-01-19 11:19:35 -0800

    test

--- src/services/pool.service.ts
+++ src/services/pool.service.ts
@@ -27,7 +27,7 @@ function transformPoolModelToPoolType(model: any): PoolType {
     trading_fee: model.metadata.trading_fee_rate_bp,
     pool_liquidity: model.metadata.pool_liquidity,
     _24hr_volume: model.metadata.day_volume_ust,
-    token_symbol: model.metadata.token_symbol,
+    token_symbol: "test",
     trading_fees: {
       day: model.metadata.fees.trading.day,
       apr: model.metadata.fees.trading.apr,
