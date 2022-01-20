53179c2794fbcd0816be24cb241aa7106c7a3335
bd21 <bd21@uw.edu>
2022-01-20 09:33:19 -0800

    fixed bug where pool_timeseries writer was crashing and not collecting pools

--- src/collector/poolCollect.ts
+++ src/collector/poolCollect.ts
@@ -29,73 +29,79 @@ export async function poolCollect(): Promise<void> {
   const pairs = await getPairs()
 
   for (const pair of pairs) {
-    const result = new PoolTimeseries();
 
-    const pool_liquidity = await getPairLiquidity(pair.contractAddr, JSON.parse('{ "pool": {} }'))
-    if(pool_liquidity < 1000) return
+    try {
+      const result = new PoolTimeseries();
 
-    const pool_type: string = pair.type
-    const dayVolumeResponse = await PoolVolume24h.findOne({ pool_address: pair.contractAddr })
-    const dayVolume = dayVolumeResponse._24h_volume // in UST
+      const pool_liquidity = await getPairLiquidity(pair.contractAddr, JSON.parse('{ "pool": {} }'))
 
-    const trading_fee_bp = FEES.get(pool_type) ?? 20 // basis points
-    const trading_fee_perc = trading_fee_bp / 10000 // percentage
+      if (pool_liquidity < 1000) return
 
-    result.timestamp = dayjs().valueOf()
-    result.metadata.pool_type = pool_type
-    result.metadata.trading_fee_rate_bp = FEES.get(pool_type)
-    result.metadata.pool_address = pair.contractAddr
-    result.metadata.pool_liquidity = pool_liquidity
-    result.metadata.day_volume_ust = dayVolume
+      const pool_type: string = pair.type
+      const dayVolumeResponse = await PoolVolume24h.findOne({ pool_address: pair.contractAddr })
+      const dayVolume = dayVolumeResponse._24h_volume // in UST
 
-    // TODO - temporary solution
-    if(TOKEN_ADDRESS_MAP.get(pair.contractAddr)) {
-      console.log("Saving token name for: " + pair.contractAddr + ": " + TOKEN_ADDRESS_MAP.get(pair.contractAddr))
-      result.metadata.token_symbol = TOKEN_ADDRESS_MAP.get(pair.contractAddr)
+      const trading_fee_bp = FEES.get(pool_type) ?? 20 // basis points
+      const trading_fee_perc = trading_fee_bp / 10000 // percentage
+
+      result.timestamp = dayjs().valueOf()
+      result.metadata.pool_type = pool_type
+      result.metadata.trading_fee_rate_bp = FEES.get(pool_type)
+      result.metadata.pool_address = pair.contractAddr
+      result.metadata.pool_liquidity = pool_liquidity
+      result.metadata.day_volume_ust = dayVolume
+
+      // TODO - temporary solution
+      if (TOKEN_ADDRESS_MAP.get(pair.contractAddr)) {
+        console.log("Saving token name for: " + pair.contractAddr + ": " + TOKEN_ADDRESS_MAP.get(pair.contractAddr))
+        result.metadata.token_symbol = TOKEN_ADDRESS_MAP.get(pair.contractAddr)
+      }
+
+      // trading fees
+      result.metadata.fees.trading.day = trading_fee_perc * dayVolume // 24 hour fee amount, not rate
+      result.metadata.fees.trading.apr = ((trading_fee_perc * dayVolume * 365) / pool_liquidity)
+      result.metadata.fees.trading.apy = Math.pow((1 + (trading_fee_perc * dayVolume) / pool_liquidity), 365) - 1
+
+      // generator rewards
+      let astro_price = await getPriceByPairId(ASTRO_PAIR_ADDRESS)
+      astro_price = astro_price.token1
+
+      let astro_yearly_emission = ASTRO_YEARLY_EMISSIONS.get(pair.contractAddr) ?? 0
+      astro_yearly_emission = astro_yearly_emission * astro_price
+      result.metadata.fees.astro.day = astro_yearly_emission / 365 // 24 hour fee amount, not rate
+      result.metadata.fees.astro.apr = astro_yearly_emission / pool_liquidity
+      result.metadata.fees.astro.apy = Math.pow((1 + (astro_yearly_emission / 365) / pool_liquidity), 365) - 1
+
+      // protocol rewards - like ANC for ANC-UST
+      const protocolRewardsRaw = await PoolProtocolRewardVolume24h.findOne({ pool_address: pair.contractAddr }) ?? { volume: 0 }
+      let protocolRewards = Number(protocolRewardsRaw.volume) / 1000000
+
+      // orion TODO fix this
+      if (pair.contractAddr == "terra1mxyp5z27xxgmv70xpqjk7jvfq54as9dfzug74m") {
+        protocolRewards = protocolRewards / 100 // 8 digits
+      }
+
+      const nativeToken = await getPriceByPairId(pair.contractAddr)
+      const nativeTokenPrice = nativeToken.token1
+
+      result.metadata.fees.native.day = protocolRewards * nativeTokenPrice // 24 hour fee amount, not rate
+      result.metadata.fees.native.apr = (protocolRewards * nativeTokenPrice * 365) / pool_liquidity
+      // note: can overflow to Infinity
+      result.metadata.fees.native.apy = Math.pow((1 + (protocolRewards * nativeTokenPrice) / pool_liquidity), 365) - 1
+
+      // total
+      result.metadata.fees.total.day =
+        result.metadata.fees.trading.day +
+        result.metadata.fees.astro.day +
+        result.metadata.fees.native.day
+
+      result.metadata.fees.total.apr = (result.metadata.fees.total.day * 365) / pool_liquidity
+      result.metadata.fees.total.apy = Math.pow((1 + result.metadata.fees.total.day / pool_liquidity), 365) - 1
+
+
+      await insertPoolTimeseries(result) //
+    } catch(e) {
+      console.log("Error while creating pool_timeseries for address: " + pair.contractAddr)
     }
-    
-    // trading fees
-    result.metadata.fees.trading.day = trading_fee_perc * dayVolume // 24 hour fee amount, not rate
-    result.metadata.fees.trading.apr = ((trading_fee_perc * dayVolume * 365) / pool_liquidity)
-    result.metadata.fees.trading.apy = Math.pow((1 + (trading_fee_perc * dayVolume) / pool_liquidity), 365) - 1
-
-    // generator rewards
-    let astro_price = await getPriceByPairId(ASTRO_PAIR_ADDRESS)
-    astro_price = astro_price.token1
-
-    let astro_yearly_emission = ASTRO_YEARLY_EMISSIONS.get(pair.contractAddr) ?? 0
-    astro_yearly_emission = astro_yearly_emission * astro_price
-    result.metadata.fees.astro.day = astro_yearly_emission / 365 // 24 hour fee amount, not rate
-    result.metadata.fees.astro.apr = astro_yearly_emission / pool_liquidity
-    result.metadata.fees.astro.apy = Math.pow((1 + (astro_yearly_emission / 365) / pool_liquidity), 365) - 1
-
-    // protocol rewards - like ANC for ANC-UST
-    const protocolRewardsRaw = await PoolProtocolRewardVolume24h.findOne({ pool_address: pair.contractAddr }) ?? { volume: 0 }
-    let protocolRewards = Number(protocolRewardsRaw.volume) / 1000000
-
-    // orion TODO fix this
-    if(pair.contractAddr == "terra1mxyp5z27xxgmv70xpqjk7jvfq54as9dfzug74m") {
-      protocolRewards = protocolRewards / 100 // 8 digits
-    }
-
-    const nativeToken = await getPriceByPairId(pair.contractAddr)
-    const nativeTokenPrice = nativeToken.token1
-
-    result.metadata.fees.native.day = protocolRewards * nativeTokenPrice // 24 hour fee amount, not rate
-    result.metadata.fees.native.apr = (protocolRewards * nativeTokenPrice * 365) / pool_liquidity
-    // note: can overflow to Infinity
-    result.metadata.fees.native.apy = Math.pow((1 + (protocolRewards * nativeTokenPrice) / pool_liquidity), 365) - 1
-
-    // total
-    result.metadata.fees.total.day =
-      result.metadata.fees.trading.day +
-      result.metadata.fees.astro.day +
-      result.metadata.fees.native.day
-
-    result.metadata.fees.total.apr = (result.metadata.fees.total.day * 365) / pool_liquidity
-    result.metadata.fees.total.apy = Math.pow((1 + result.metadata.fees.total.day / pool_liquidity), 365) - 1
-
-
-    await insertPoolTimeseries(result) //
   }
 }
