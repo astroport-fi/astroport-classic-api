c8518d49eb7cb6a73d8aa81639452ebe8001732e
bd21 <bd21@uw.edu>
2022-03-22 04:09:22 -0700

    fix aggregate pool volume

--- src/aggregator/aggregatePoolVolume.ts
+++ src/aggregator/aggregatePoolVolume.ts
@@ -4,6 +4,7 @@ import { getLastHeight, getPairs } from "../services";
 import { TERRA_CHAIN_ID } from "../constants";
 import { PoolVolume } from "../models/pool_volume.model";
 import { PoolVolume24h } from "../models/pool_volume_24h.model";
+import { getLatestBlock } from "../lib/terra";
 
 dayjs.extend(utc);
 
@@ -12,19 +13,19 @@ dayjs.extend(utc);
  * Update the pool_volume_24h table
  */
 
-const DIGITS = 1000000;
 const chainId = TERRA_CHAIN_ID
 const BLOCKS_PER_YEAR = 4656810;
 
 export async function aggregatePoolVolume(): Promise<void> {
 
   // get latest block height
-  const latestHeight = await getLastHeight(chainId)
-  console.log("latest height: " + latestHeight.value)
+  const { height, time } = await getLatestBlock()
+  const latestHeight = Number(height)
+  console.log("latest height: " + latestHeight)
 
   // get block height 24hs ago
   // TODO - an estimation - switch over when height data correctly indexed
-  const startBlockHeight = latestHeight.value - Math.floor(BLOCKS_PER_YEAR / 365)
+  const startBlockHeight = latestHeight - Math.floor(BLOCKS_PER_YEAR / 365)
   // const startBlockHeight = await getHeightByDate(
   //   chainId,
   //   dayjs().utc().subtract(1, 'y').toISOString());
@@ -36,7 +37,7 @@ export async function aggregatePoolVolume(): Promise<void> {
   for (const element of pairs) {
     const pool_volumes = await PoolVolume.find(
       { poolAddress: element.contractAddr,
-              block: { $gt: startBlockHeight, $lt: latestHeight.value }}
+              block: { $gt: startBlockHeight, $lt: latestHeight }}
     );
     let sum = 0;
     pool_volumes.forEach((element) => {
@@ -48,11 +49,11 @@ export async function aggregatePoolVolume(): Promise<void> {
     if(prev) {
       await PoolVolume24h.findOneAndUpdate(
         { pool_address: element.contractAddr },
-        { block: latestHeight.value, _24h_volume: sum });
+        { block: latestHeight, _24h_volume: sum });
     } else {
       await PoolVolume24h.create({
           pool_address: element.contractAddr,
-          block: latestHeight.value,
+          block: latestHeight,
           _24h_volume: sum
         });
     }
