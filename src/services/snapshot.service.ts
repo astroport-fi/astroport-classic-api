580730ba81ad9233e655b049bf5b8ae2234897ce
bd21 <bd21@uw.edu>
2022-03-28 01:16:12 -0700

    lower page size for snapshots to 20

--- src/services/snapshot.service.ts
+++ src/services/snapshot.service.ts
@@ -2,9 +2,9 @@ import { Supply } from "../models/supply.model";
 import { Supply as SupplyType } from "../types/supply.type";
 import { Snapshot } from "../models/snapshot.model.ts";
 
-export async function getSnapshots(limit = 100, offset = 0): Promise<any> {
-  if(limit > 100) {
-    limit = 100
+export async function getSnapshots(limit = 20, offset = 0): Promise<any> {
+  if(limit > 20) {
+    limit = 20
   }
   const result = await Snapshot.find().skip(offset).sort({ block: -1 }).limit(limit)
 
