3651527412d480f056a4bc1adfbb076a10d57ff5
Swaleh S <miclondev@gmail.com>
2022-04-19 22:25:33 +0200

    add cached connection

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -1,11 +1,46 @@
-import mongoose from "mongoose";
-
+import mongoose, { Connection } from "mongoose";
 import { MONGODB_URL } from "../constants";
+
+if (!MONGODB_URL) {
+  throw new Error("Please define the MONGODB_URL environment variable inside .env.local");
+}
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: any;
+}
+
+/**
+ * Global is used here to maintain a cached connection.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export const connectToDatabase = (): Promise<void> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts: mongoose.ConnectOptions = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = cached.promise;
+  return cached.conn;
+};
 
 mongoose.Promise = global.Promise;
 let isConnected: number;
 
-export const connectToDatabase = (): Promise<void> => {
+export const connectToDatabasev1 = (): Promise<void> => {
   if (MONGODB_URL == null) {
     return Promise.reject();
   }
