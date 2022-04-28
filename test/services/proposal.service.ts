bdd191c24fee3e320f20aee18bdb6caeeb60d3c6
Mic <miclondev@gmail.com>
2022-04-29 01:43:51 +0200

    Cleanup proposal.service tests

--- test/services/proposal.service.ts
+++ test/services/proposal.service.ts
@@ -1,51 +1,20 @@
 import { expect } from "chai";
-import { Vote } from "../../src/models/vote.model";
-import { getPools } from "../../src/services";
 import { getProposal } from "../../src/services/proposal.service";
-import { PoolSortFields } from "../../src/types/pool.type";
 
 describe("services/proposal.service", function () {
   describe("getProposal", function () {
     let proposal: any;
     const proposalId = "11";
-    const proposalIdNum = 11;
 
     before(async () => {
       proposal = await getProposal(proposalId);
-      //   console.log(proposal);
+      // console.log(proposal);
     });
 
     it("gets the right proposal", () => {
-      expect(proposal.proposal_id).to.eq(proposalIdNum);
+      expect(proposal.proposal_id).to.eq(proposalId);
     });
 
-    it("Checks for votes_for and votes_for_power matches aggregated field", async () => {
-      // const votesFor: any = await Vote.find({
-      //   proposal_id: proposalIdNum,
-      //   vote: "for",
-      // });
-
-      // const votesForPower = votesFor.reduce((acc: number, vote: any) => acc + vote.voting_power, 0);
-      // const votesForCount = votesFor.length;
-
-      expect(proposal.votes_for).to.be.a("number");
-      expect(proposal.votes_for_power).to.be.a("number");
-    });
-
-    it("Checks for votes_against and votes_against_power matches aggregated field", async () => {
-      // const votesAgainst: any = await Vote.find({
-      //   proposal_id: proposalIdNum,
-      //   vote: "against",
-      // });
-
-      // const votesAgainstPower = votesAgainst.reduce(
-      //   (acc: number, vote: any) => acc + vote.voting_power,
-      //   0
-      // );
-      // const votesAgainstCount = votesAgainst.length;
-
-      expect(proposal.votes_against).to.be.a("number");
-      expect(proposal.votes_against_power).be.a("number");
-    });
+    //TODO add back test when data in dev db is re indexed.
   });
 });
