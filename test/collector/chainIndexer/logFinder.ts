3c13a41a417c9da98ac3ed8bb265e30003afa815
Donovan Solms <donovan.solms@gmail.com>
2022-05-13 12:26:00 +0200

    Add xAstro and vxAstro log finder tests

--- test/collector/chainIndexer/logFinder.ts
+++ test/collector/chainIndexer/logFinder.ts
@@ -5,6 +5,8 @@ import { calculateThirdPartyApr } from "collector/chainIndexer/calculateApr";
 import { createAstroNativeFeeLogFinder } from "collector/logFinder/createAstroNativeFeeLogFinder";
 import { getTxBlock } from "lib/terra";
 import { ScheduleType } from "types/contracts";
+import { vxAstroCreateLockLogFinder } from "collector/logFinder/vxAstroCreateLockLogFinder";
+import { xAstroMintLogFinder } from "collector/logFinder/xAstroMintLogFinder";
 
 describe("findXAstroFees", function () {
   it("Should get XAstroFees event from transfer log", async () => {
@@ -18,6 +20,39 @@ describe("findXAstroFees", function () {
     const foundLog = astroNativeFeeLogFound.find(() => true)?.transformed;
     expect(foundLog?.amount).to.be.a("number");
     expect(foundLog?.token).to.be.a("string");
+  });
+});
+
+describe("findvxLock", function () {
+  it("Should get a new create_lock event for vxAstro from transfer log", async () => {
+    const event = await getEvent(
+      8974586,
+      "F5852B3A7D15C01653C1EC42233DDC1BE6B34A675B8C3C6CCD38BAA43CD3F719",
+      "wasm"
+    );
+
+    const vxastroCreateLockLogFinder = vxAstroCreateLockLogFinder();
+    const vxastroCreateLockLogFound = vxastroCreateLockLogFinder(event);
+
+    const foundLog = vxastroCreateLockLogFound.find(() => true)?.transformed;
+    expect(foundLog?.from).to.be.a("string");
+  });
+});
+
+describe("findxAstro", function () {
+  it("Should get a mint fro xAstro event from transfer log", async () => {
+    const event = await getEvent(
+      8763516,
+      "E2A576F5699EB4F45A0AEDD4EC207B57F508F934984C7E2C5294DF066FEFE79A",
+      "wasm"
+    );
+
+    const xastroMintLogFinder = xAstroMintLogFinder();
+    const xastroMintLogFound = xastroMintLogFinder(event);
+
+    const foundLog = xastroMintLogFound.find(() => true)?.transformed;
+    expect(foundLog?.to).to.be.a("string");
+    expect(foundLog?.amount).to.be.a("number");
   });
 });
 
